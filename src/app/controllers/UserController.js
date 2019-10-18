525cbe83e78f8fa97291f018ba86d1edf2665b5c
Gabriel Monteiro da Costa e Costa <gabriel.costa@implantainformatica.com.br>
2019-10-18 16:30:01 -0300

    upload images - init

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -68,7 +68,7 @@ class UserController {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name, provider } = await user.update(req.body);
+    const { id, name, provider } = await User.update(req.body);
 
     return res.json({
       id,
